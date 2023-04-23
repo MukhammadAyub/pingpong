a7f42026d6399c42f1b16dc7a1f1af351f7aca06
MukhammadAyub <47212753+MukhammadAyub@users.noreply.github.com>
2023-04-23 14:52:37 +0500

    Update main.py
    version 1.1

--- main.py
+++ main.py
@@ -4,12 +4,54 @@ window = display.set_mode((700, 500))
 background = transform.scale(image.load('background.jpg'), (700, 500))
 clock = time.Clock()
 
+class Gamesprite(sprite.Sprite):
+    def __init__(self, player_image, x, y, width, height, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(Gamesprite):
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        if keys[K_DOWN] and self.rect.y < 395:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        if keys[K_s] and self.rect.y < 395:
+            self.rect.y += self.speed
+
+racket1 = Player('racket.png', 10, 200, 30, 100, 15)
+racket2 = Player('racket.png', 660, 200, 30, 100, 15)
+ball = Gamesprite('tenis_ball.png', 325, 200, 50, 50, 7)
+
 game = True
 while game:
     window.blit(background, (0,0))
     for e in event.get():
         if e.type == QUIT:
             game = False
+    
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+
+    racket1.update_l()
+    racket2.update_r()
 
     clock.tick(60)
     display.update()
